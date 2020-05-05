3be57fc318399e055b9144259b4e32ae26f0d5af
devlinpadayachee@gmail.com <devlinpadayachee@gmail.com>
2020-05-05 04:00:18 +0200

    posst feature picture

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -33,6 +33,7 @@ const typeDefs = gql`
         login(email: String!, password: String!): LoginResponse!
         getResetPasswordLink(email: String!): JSON!
         resetPassword(resetToken: String!, password: String!): UserUpdatedResponse!
+        sendUserToUserEmailMessage(to: ID!, from: ID!, message: String!): JSON!
         createUser(user: UserCreateInput): User!
         updateUser(user: UserUpdateInput): UserUpdatedResponse! 
         deleteUser(id: ID!): UserDeletedResponse! 
--- src/utils.js
+++ src/utils.js
@@ -79,6 +79,7 @@ module.exports.createMongoInstance = async () => {
   const postSchema = new Schema({
     title: {type: String, required: true},
     content: {type: String, required: true},
+    featurePicture: {type: String},
     createdBy: {type: Schema.Types.ObjectId, required: true},
     createdAt: Number,
     updatedAt: Number,
@@ -307,6 +308,159 @@ module.exports.getArenaConfig = () => {
   });
   return arenaConfig;
 };
+
+module.exports.getUserToUserMailTemplate = (toUser, fromUser, message) => {
+
+return `<!doctype html>
+  <html>
+    <head>
+        <meta charset="utf-8">
+        <title>OnlineTeeBox Message</title>
+        
+        <style>
+        .message-box {
+            max-width: 800px;
+            margin: auto;
+            padding: 30px;
+            border: 1px solid #eee;
+            box-shadow: 0 0 10px rgba(0, 0, 0, .15);
+            font-size: 16px;
+            line-height: 24px;
+            font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+            color: #555;
+        }
+        
+        .message-box table {
+            width: 100%;
+            line-height: inherit;
+            text-align: left;
+        }
+        
+        .message-box table td {
+            padding: 5px;
+            vertical-align: top;
+        }
+        
+        .message-box table tr td:nth-child(2) {
+            text-align: right;
+        }
+        
+        .message-box table tr.top table td {
+            padding-bottom: 20px;
+        }
+        
+        .message-box table tr.top table td.title {
+            font-size: 45px;
+            line-height: 45px;
+            color: #333;
+        }
+        
+        .message-box table tr.information table td {
+            padding-bottom: 40px;
+        }
+        
+        .message-box table tr.heading td {
+            background: #eee;
+            border-bottom: 1px solid #ddd;
+            font-weight: bold;
+            padding: 30px;
+        }
+        
+        .message-box table tr.details td {
+            padding-bottom: 20px;
+        }
+        
+        .message-box table tr.item td{
+            border-bottom: 1px solid #eee;
+        }
+        
+        .message-box table tr.item.last td {
+            border-bottom: none;
+        }
+        
+        .message-box table tr.total td:nth-child(2) {
+            border-top: 2px solid #eee;
+            font-weight: bold;
+        }
+        
+        @media only screen and (max-width: 600px) {
+            .message-box table tr.top table td {
+                width: 100%;
+                display: block;
+                text-align: center;
+            }
+            
+            .message-box table tr.information table td {
+                width: 100%;
+                display: block;
+                text-align: center;
+            }
+        }
+        
+        /** RTL **/
+        .rtl {
+            direction: rtl;
+            font-family: Tahoma, 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+        }
+        
+        .rtl table {
+            text-align: right;
+        }
+        
+        .rtl table tr td:nth-child(2) {
+            text-align: left;
+        }
+        </style>
+    </head>
+    â€‹
+    <body>
+        <div class="message-box">
+            <table cellpadding="0" cellspacing="0">
+                <tr class="top">
+                    <td colspan="2">
+                        <table>
+                            <tr>
+                                <td class="title">
+                                    <img src="https://nuhome-graphql-client.herokuapp.com/img/logo.ff72e2e1.png" style="width:100%; max-width:100px;">
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+
+                <tr class="information">
+                    <td colspan="2">
+                        <table>
+                            <tr>
+                                <td>
+                                  ${fromUser.firstName} ${fromUser.lastName} sent you a message:<br>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr class="heading" colspan="2">
+                    <td>
+                      ${message}
+                    </td>
+                </tr>        
+                <tr class="information">
+                    <td colspan="2">
+                        <table>
+                            <tr>
+                                <td>
+                                  You can get back to me at <a href="${process.env.APP_CLIENT_URL}">Appfrica</a><br> 
+                                  or contact me directly at ${fromUser.email}
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </body>
+  </html>`
+  }
 
 
 

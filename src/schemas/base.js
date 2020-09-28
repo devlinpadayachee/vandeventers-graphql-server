daeaddca79fc748b1d0274cfdc89cab967d2f410
devlinpadayachee <devlinpadayachee@gmail.com>
2020-09-28 22:13:07 +0200

    updated SErvice Acccount

--- src/schemas/base.js
+++ src/schemas/base.js
@@ -34,6 +34,14 @@ const typeDefs = gql`
     type AckResponse {
         success: Boolean!
     }
+
+    type Address {
+        addressLine1: String!
+        addressLine2: String
+        city: String!
+        country: String!
+        postalCode: String!
+    }
         
 `;
 
